2d808e176841bd61591361fdd263e3f1de51d7cd
crnsimon <simon1chr@yahoo.com>
2024-03-08 07:40:24 +0100

    finallyy

---
+++ 3d.py
@@ -0,0 +1,83 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+def rotation(rotx, roty, rotz):
+    rotx, roty, rotz = np.radians(rotx), np.radians(roty), np.radians(rotz)
+    rotx = np.array([[1, 0, 0], [0, np.cos(rotx), -np.sin(rotx)], [0, np.sin(rotx), np.cos(rotx)]])
+    roty = np.array([[np.cos(roty), 0, np.sin(roty)], [0, 1, 0], [-np.sin(roty), 0, np.cos(roty)]])
+    rotz = np.array([[np.cos(rotz), -np.sin(rotz), 0], [np.sin(rotz), np.cos(rotz), 0], [0, 0, 1]])
+    rot = np.dot(np.dot(rotx, roty), rotz)
+    return rot  
+
+def translation(x, y, z):
+    return np.array([[x],[y],[z]])
+
+# def compound(x, y, z, rotx, roty, rotz):
+#     total =  np.vstack([np.hstack([rotation(rotx, roty, rotz), translation(x, y, z)]), np.array([0, 0, 0, 1])])
+#     return total
+
+def compound(rotmatrix, transvector):
+    total =  np.vstack([np.hstack([rotmatrix, transvector]), np.array([0, 0, 0, 1])])
+    return total
+
+# Define parameters
+l0 = 2  # cm
+l1 = 1  # cm
+l2 = 9.525   # cm (3.75”)
+l3 = 10.795  # cm (4.25”)
+l3 =  8.5725 # cm (3.375”)
+l4 = 6  # cm
+
+
+# Create RGB axis data
+length = 5
+axis_system = np.array([[length,0,0], [0,length,0],[0,0,length]])
+origin = np.array([0,0,0,0,0,0])
+joint0 = np.array([0,0,l0,0,0,0])
+joint1 = np.array([0,0,l1,0,0,45])
+joint2 = np.array([0,0,l2,-45,0,0])
+joint3 = np.array([0,0,l3,90,0,0])
+EE = np.array([0,0,l4,35,0,0])
+names = ['origin', 'joint0', 'joint1', 'joint2', 'joint3', 'EE']
+
+
+# Create figure and 3D axis
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+colors=['r', 'g', 'b']
+latest_pos = np.array([[0],[0],[0]])
+latest_rot = np.array([[1,0,0],[0,1,0],[0,0,1]])
+namecount = 0
+
+# Plot each axis system at its respective position
+for pos in origin, joint0,joint1,joint2,joint3,EE:
+    old_pos = latest_pos
+    latest_rot = np.dot(latest_rot,rotation(pos[3],pos[4],pos[5]))
+    latest_pos = np.dot(latest_rot,np.array([[pos[0]],[pos[1]],[pos[2]]])) + latest_pos
+    compound_matrix = compound(latest_rot,latest_pos)
+    new_pos = np.dot(compound_matrix,np.vstack([np.array([[0],[0],[0]]),[1]]))[:-1]
+    ax.plot([old_pos[0][0], new_pos[0][0]], [old_pos[1][0], new_pos[1][0]], [old_pos[2][0], new_pos[2][0]], color='black')
+    ax.text(new_pos[0][0], new_pos[1][0], new_pos[2][0], names[namecount])
+    namecount += 1
+    for axis in axis_system:
+        new_axis = np.dot(compound_matrix,np.vstack([*axis, np.array([1])]))[:-1]
+        ax.quiver(*new_pos,*(new_axis-new_pos), color=colors[axis_system.tolist().index(axis.tolist())])
+    latest_pos = new_pos
+    
+
+# Set axis limits
+box_size = 15
+ax.set_xlim(-box_size, box_size)
+ax.set_ylim(-box_size, box_size)
+ax.set_zlim(0, 2*box_size)
+        
+# ax.set_box_aspect([1,1,1])
+
+# Set plot title
+plt.title('RGB Axis System')
+
+# Show plot
+plt.show()
+
+
---
+++ main.py
@@ -0,0 +1,70 @@
+import math
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+import numpy as np
+
+def calculate_expression(l1, l2, l3, q1, q2, q3, r0):
+    x = (l1 * math.sin(q1) + l2 * math.sin(q1 + q2) + l3 * math.sin(q1 + q2 + q3)) * math.cos(r0)
+    y = (l1 * math.sin(q1) + l2 * math.sin(q1 + q2) + l3 * math.sin(q1 + q2 + q3)) * math.sin(r0)
+    z = l1 * math.cos(q1) + l2 * math.cos(q1 + q2) + l3 * math.cos(q1 + q2 + q3)
+    return x, y, z
+
+def inverse_kinematics(l1, l2, l3, x, y, z):
+    r0 = math.atan2(y, x)
+    y = y / math.sin(r0)
+    x = x / math.cos(r0)
+    
+    return math.degrees(q1), math.degrees(q2), math.degrees(q3), math.degrees(r0)
+
+
+l1 =  9.525  # cm (3.75”)
+l2 = 10.795  # cm (4.25”)
+l3 =  8.5725 # cm (3.375”)
+
+# listy = []
+# listz = []
+
+# r0 = math.radians(90)  # radians
+# for i in range(0, 360, 10):
+#     for j in range(0, 360, 10):
+#         for k in range(0, 360, 10):
+#             q1 = math.radians(i)
+#             q2 = math.radians(j)
+#             q3 = math.radians(k)
+#             x, y, z = calculate_expression(l1, l2, l3, q1, q2, q3, r0)
+#             listy.append(y)
+#             listz.append(z)
+# plt.plot(listy, listz, linewidth=0.1)
+# plt.xlabel('y - [cm]')
+# plt.ylabel('z - [cm]')
+# plt.axis('square')
+# plt.savefig('subspace_no_limits.eps', format='eps', dpi=1000)
+# plt.show()
+
+# listx = []
+# listy = []
+# listz = []
+
+# for r0 in range(0, 360, 5):
+#     for i in range(0, 360, 30):
+#         for j in range(0, 360, 30):
+#             for k in range(0, 360, 30):
+#                 q1 = math.radians(i)
+#                 q2 = math.radians(j)
+#                 q3 = math.radians(k)
+#                 x, y, z = calculate_expression(l1, l2, l3, q1, q2, q3, r0)
+#                 listx.append(x)
+#                 listy.append(y)
+#                 listz.append(z)
+# fig = plt.figure()
+# ax = plt.axes(projection='3d')
+# ax.plot3D(listx, listy, listz)
+# plt.show()
+
+# for i in [[0.1,0.1,0.1],[0.2,0.1,0.3],[0.0,0.0,0.3],[0.0,0.0,0.07]]:
+#     x, y, z = i
+#     q1, q2, q3, r0 = inverse_kinematics(l1, l2, l3, x, y, z)
+#     print(f'x: {x}, y: {y}, z: {z}, q1: {q1}, q2: {q2}, q3: {q3}, r0: {r0}')
+#     x, y = calculate_expression(l1, l2, l3, math.radians(q1), math.radians(q2), math.radians(q3), math.radians(r0))
+#     print(f'x: {x}, y: {y}, z: {z}, q1: {q1}, q2: {q2}, q3: {q3}, r0: {r0}')
+
