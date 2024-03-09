cf610a75e834cbe05412c02ab67d3d4e51f698ba
arham <M.A.Elahi@student.tudelft.nl>
2024-03-09 17:27:53 +0100

    Added Inverse Kinematics

--- IK.py
+++ IK.py
@@ -9,27 +9,35 @@ l2 = 0.09525   # m (3.75”)
 l3 = 0.10795   # m (4.25”)
 l4 = 0.085725  # m (3.375”)
 
-x, y, z = (0.0, 0.0, 0.07)
+
+x, y, z = (0, 0, 0.03)
 def equations(qs):
-    q1, q2, q3, q4 = qs
-    eq1 = np.cos(q1)*(l1x-l2*np.sin(q2) + l3*np.cos(q2+q3) + l4*np.cos(q2+q3+q4)) - x
-    eq2 = np.sin(q1)*(l1x-l2*np.sin(q2) + l3*np.cos(q2+q3) + l4*np.cos(q2+q3+q4)) - y
-    eq3 = l0 + l1 + l2*np.cos(q2) + l3*np.cos(q2-q3) + l4*np.cos(q2-q3-q4) - z
+    q0, q1, q2, q3 = qs
+    eq1 = np.cos(q0)*(l1x+l2*np.sin(q1) + l3*np.sin(q1+q2) + l4*np.sin(q1+q2+q3)) - x
+    eq2 = np.sin(q0)*(l1x+l2*np.sin(q1) + l3*np.sin(q1+q2) + l4*np.sin(q1+q2+q3)) - y
+    eq3 = l0 + l1 + l2*np.cos(q1) + l3*np.cos(q1+q2) + l4*np.cos(q1+q2+q3) - z
     return (eq1,eq2,eq3)
 
 def FK(qs):
-    q1, q2, q3, q4 = qs
-    x = np.cos(q1)*(l1x-l2*np.sin(q2) + l3*np.cos(q2+q3) + l4*np.cos(q2+q3+q4))
-    y = np.sin(q1)*(l1x-l2*np.sin(q2) + l3*np.cos(q2+q3) + l4*np.cos(q2+q3+q4))
-    z = l0 + l1 + l2*np.cos(q2) + l3*np.cos(q2-q3) + l4*np.cos(q2-q3-q4)
+    q0, q1, q2, q3 = qs
+    x = np.cos(q0)*(l1x+l2*np.sin(q1) + l3*np.sin(q1+q2) + l4*np.sin(q1+q2+q3))
+    y = np.sin(q0)*(l1x+l2*np.sin(q1) + l3*np.sin(q1+q2) + l4*np.sin(q1+q2+q3))
+    z = l0 + l1 + l2*np.cos(q1) + l3*np.cos(q1+q2) + l4*np.cos(q1+q2+q3)
     return x, y, z
 
-res = least_squares(equations, (0, 0, 20*np.pi/180, 0), bounds = ((-np.pi/2, -np.pi/3, 0, -np.pi/2), (np.pi/2, np.pi/3, 70*np.pi/180, np.pi/2)))
-q1, q2, q3, q4 = res.x * 180/np.pi
-print(FK(res.x))
-print(q1, q2, q3, q4)
 
-# 1 45.00000039433621 -24.979112060238233 58.291236860903645 56.02913713165773
+res = least_squares(equations, (0, 0, 120*np.pi/180, 0), bounds = ((-np.pi/2, -np.pi/3, np.pi/2, -np.pi/2), (np.pi/2, np.pi/3, 160*np.pi/180, np.pi/2)))
+print(res.cost)
+print(FK(res.x))
+print((res.x * 180/np.pi))
+
+# 1a 44.99999540158393 -35.39631401076471 131.14051001614993 16.780832836585795
+# 1b 45.00000000568893 -35.989212300338195 145.06289823013645 -13.85783524584236
+# 1c 44.99999998019838 -32.17168302076569 124.18255894921438 27.96954621515868
+# 1d 45.0000003105869 -36.73469010945322 135.43001894767596 8.970677559754671
+# 1e 44.999999783643396 -37.247777842899175 138.79152190824337 2.0984447312954426
+# 1f 45.00000001211157 -37.15681085316987 141.53425994687646 -4.226757242076513
+# 1g 45.000000479526804 -36.69527782835328 143.48688505037603 -9.292542699460709
 # 2 26.5650542060046 -15.071944527609885 25.76090878960464 4.419201679989688
 # 3 0 20.70996636189051 63.244699362791515 -2.2143191395548176
 # 4 Unfeasible
