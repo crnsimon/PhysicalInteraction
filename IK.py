3fedb7d035113a14ad09ac71b213b52af7a62afe
arham <M.A.Elahi@student.tudelft.nl>
2024-03-29 00:04:27 +0100

    Random new things

--- IK.py
+++ IK.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy.optimize import least_squares
 import matplotlib.pyplot as plt
 
-sim = False
+sim = True
 
 if sim:
     # Define parameters
@@ -14,9 +14,9 @@ if sim:
     l4 = 0.075  # m (3.375”)
 else:
     # Define parameters
-    l0 = 0.045  # m
-    l1 = 0.019  # m
-    l1x = 0.013  # m
+    l0 = 0.041  # m
+    l1 = 0.025  # m
+    l1x = 0.01  # m
     l2 = 0.09525  # m (3.75”)
     l3 = 0.10795  # m (4.25”)
     l4 = 0.085725  # m (3.375”)
@@ -48,8 +48,10 @@ def FK(qs):
 
 def orientation_graph(angles):
     ans = np.zeros((5, np.size(angles)))
-    for i, orientation in enumerate(angles):
-        res = least_squares(equations_orientation, (0, 0, 100*np.pi/180, 0), bounds = ((-np.pi/2, -np.pi/3, np.pi/2, -np.pi/2), (np.pi/2, np.pi/3, 160*np.pi/180, np.pi/2)))
+    global orientation
+    for i, orient in enumerate(angles):
+        orientation = orient
+        res = least_squares(equations_orientation, (0, 0, 90*np.pi/180, 0), bounds=((-np.pi/2, -np.pi/3, 70*np.pi/180, -np.pi/2), (np.pi/2, np.pi/3, 150*np.pi/180, np.pi/2)))
         ans[:4, i] = res.x * 180/np.pi
         ans[4, i] = sum((np.array([x, y, z]) - np.array(FK(res.x)))**2)*100000
     for i in range(4):
@@ -82,14 +84,26 @@ def inverse_kinematics(x0, orient=None, printing=False):
     x = FK(res.x)
     if printing:
         print(FK(res.x), sum([res.x[1], res.x[2], res.x[3]]) * 180 / np.pi)
-        print(', '.join(str(x*180/np.pi) for x in qs))
+        print(', '.join(str(x*180/np.pi) for x in res.x))
     return res.x, x
 
+
+def elbow_down(x0, orient=None, printing=False):
+    global x, y, z, orientation
+    x, y, z = x0
+    orientation = orient
+    if orient is not None:
+        res = least_squares(equations_orientation, (0, 100 * np.pi / 180, -50 * np.pi / 180, -50 * np.pi / 180))
+    else:
+        res = least_squares(equations, (0, 0, -90 * np.pi / 180, 0))
+    x = FK(res.x)
+    if printing:
+        print(res.cost)
+        print(FK(res.x), sum([res.x[1], res.x[2], res.x[3]]) * 180 / np.pi)
+        print(', '.join(str(x*180/np.pi) for x in res.x))
+    return res.x, x
+
+
 if __name__ == '__main__':
-    qs = inverse_kinematics((0, 0, 0.3),printing=True)
-
-
-# 1 0.3217505543965733, 0.37462937171086075, 0.8840461193694984, 0.509416747643335
-# 2 0.3217505543770969, 0.5340738075978947, 0.49687699432751753, -0.037196813271319316
-# 3 -0.32175055437718225, 0.5340738075979996, 0.496876994327192, -0.03719681327174962
-# 4 -0.3217505543967424, 0.37462937171281363, 0.884046119370625, 0.509416747642509
+    x0 = (0.1, 0.1, 0.1)
+    elbow_down(x0, orient=90/180*np.pi, printing=True)
