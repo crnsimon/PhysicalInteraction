aaf22c1ad55ece2d7bb0a64ff99a95a6e6e0b051
arham <M.A.Elahi@student.tudelft.nl>
2024-03-29 14:18:27 +0100

    Added another orientation graph and other things

--- IK.py
+++ IK.py
@@ -46,19 +46,42 @@ def FK(qs):
     zf = l0 + l1 + l2*np.cos(q1) + l3*np.cos(q1+q2) + l4*np.cos(q1+q2+q3)
     return xf, yf, zf
 
-def orientation_graph(angles):
+def orientation_graph(x0, angles):
     ans = np.zeros((5, np.size(angles)))
-    global orientation
+    global x, y, z, orientation
+    x, y, z = x0
+    fig, ax1 = plt.subplots()
     for i, orient in enumerate(angles):
         orientation = orient
-        res = least_squares(equations_orientation, (0, 0, 90*np.pi/180, 0), bounds=((-np.pi/2, -np.pi/3, 70*np.pi/180, -np.pi/2), (np.pi/2, np.pi/3, 150*np.pi/180, np.pi/2)))
+        res = least_squares(equations_orientation, (0, 0, 90*np.pi/180, 0), bounds=((-np.pi/2, -np.pi/3, 50*np.pi/180, -np.pi/2), (np.pi/2, np.pi/3, 150*np.pi/180, np.pi/2)))
         ans[:4, i] = res.x * 180/np.pi
-        ans[4, i] = sum((np.array([x, y, z]) - np.array(FK(res.x)))**2)*100000
+        ans[4, i] = sum((np.array([x, y, z, orientation]) - np.array([*FK(res.x), sum([res.x[1], res.x[2], res.x[3]])]))**2)**0.5
+    for i in range(4):
+        ax1.plot(angles*180/np.pi, ans[i], label=f"q{i} [deg]")
+    ax2 = ax1.twinx()
+    ax2.plot(angles*180/np.pi, ans[4], label='Error', linestyle='--', color='purple')
+    ax1.legend()
+    ax2.legend()
+    ax1.set_xlabel('Orientation [deg]')
+    ax1.set_ylabel('Joint Angle [deg]')
+    ax2.set_ylabel('RMS Error')
+    ax1.grid()
+    plt.tight_layout()
+    plt.show()
+
+def orientation_graph_no_limit(x0, angles):
+    ans = np.zeros((4, np.size(angles)))
+    global x, y, z, orientation
+    x, y, z = x0
+    for i, orient in enumerate(angles):
+        orientation = orient
+        res = least_squares(equations_orientation, (0, 0, 90*np.pi/180, 0))
+        ans[:4, i] = res.x * 180/np.pi
     for i in range(4):
         plt.plot(angles*180/np.pi, ans[i], label=f"q{i} [deg]")
-    plt.plot(angles*180/np.pi, ans[4], label='Error', linestyle='--')
     plt.legend()
     plt.xlabel('Orientation [deg]')
+    plt.ylabel('Joint Angle [deg]')
     plt.grid()
     plt.show()
 
@@ -75,35 +98,38 @@ def inverse_kinematics(x0, orient=None, printing=False):
     orientation = orient
     if orient is not None:
         res = least_squares(equations_orientation, (0, 0, 90 * np.pi / 180, 0), bounds=(
-            (-np.pi / 2, -np.pi / 3, 70 * np.pi / 180, -np.pi / 2),
+            (-np.pi / 2, -np.pi / 3, 50 * np.pi / 180, -np.pi / 2),
             (np.pi / 2, np.pi / 3, 150 * np.pi / 180, np.pi / 2)))
     else:
         res = least_squares(equations, (0, 0, 90 * np.pi / 180, 0), bounds=(
-            (-np.pi / 2, -np.pi / 3, 70 * np.pi / 180, -np.pi / 2),
+            (-np.pi / 2, -np.pi / 3, 50 * np.pi / 180, -np.pi / 2),
             (np.pi / 2, np.pi / 3, 150 * np.pi / 180, np.pi / 2)))
-    x = FK(res.x)
+    qs = res.x
+    x = FK(qs)
     if printing:
-        print(FK(res.x), sum([res.x[1], res.x[2], res.x[3]]) * 180 / np.pi)
-        print(', '.join(str(x*180/np.pi) for x in res.x))
-    return res.x, x
+        print(FK(qs), sum([qs[1], qs[2], qs[3]]) * 180 / np.pi)
+        print(', '.join(str(q*180/np.pi) for q in qs))
+    return qs, x
 
 
-def elbow_down(x0, orient=None, printing=False):
+def ik_no_limits(x0, orient=None, printing=False):
     global x, y, z, orientation
     x, y, z = x0
     orientation = orient
     if orient is not None:
-        res = least_squares(equations_orientation, (0, 100 * np.pi / 180, -50 * np.pi / 180, -50 * np.pi / 180))
+        res = least_squares(equations_orientation, (0, 0, -90 * np.pi / 180, 0))
     else:
         res = least_squares(equations, (0, 0, -90 * np.pi / 180, 0))
     x = FK(res.x)
     if printing:
-        print(res.cost)
         print(FK(res.x), sum([res.x[1], res.x[2], res.x[3]]) * 180 / np.pi)
         print(', '.join(str(x*180/np.pi) for x in res.x))
     return res.x, x
 
 
 if __name__ == '__main__':
-    x0 = (0.1, 0.1, 0.1)
-    elbow_down(x0, orient=90/180*np.pi, printing=True)
+    x0 = (0, 0, 0.3)
+    qs, x = ik_no_limits(x0, orient=0, printing=True)
+    qsim = angles_to_sim(qs)
+    print(qsim)
+
